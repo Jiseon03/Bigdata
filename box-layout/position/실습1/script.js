e32839e08287c796512617c5ec34a8d1f45923b9
Jiseon03 <wltjsqhgus0905@gmail.com>
2024-05-30 12:55:57 +0900

    📝 코드

--- box-layout/position/실습1/script.js
+++ box-layout/position/실습1/script.js
@@ -1,2 +1,11 @@
 let popupOpenBtn = document.querySelector('.popup-open-btn');
-console.log(popupOpenBtn);
+let popupCloseBtn = document.querySelector('.popup-close-btn');
+let popup = document.querySelector('.popup');
+
+popupOpenBtn.addEventListener('click',function(){
+  popup.classList.add('is-active')
+})
+
+popupCloseBtn.addEventListener('click',function(){
+  popup.classList.remove('is-active')
+})
