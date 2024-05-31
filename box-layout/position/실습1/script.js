129ba96088af12f971c8acb7c4a3b541e64e5d1c
Jiseon03 <wltjsqhgus0905@gmail.com>
2024-05-31 10:25:09 +0900

    📝 사이드바 기능 추가

--- box-layout/position/실습1/script.js
+++ box-layout/position/실습1/script.js
@@ -1,3 +1,4 @@
+//팝업창 열고 닫기
 let popupOpenBtn = document.querySelector('.popup-open-btn');
 let popupCloseBtn = document.querySelector('.popup-close-btn');
 let popup = document.querySelector('.popup');
@@ -8,4 +9,18 @@ popupOpenBtn.addEventListener('click',function(){
 
 popupCloseBtn.addEventListener('click',function(){
   popup.classList.remove('is-active')
+})
+
+
+//sidebar 
+let sidebar = document.querySelector('.sidebar');
+let sidebarOpenBtn = document.querySelector('.sidebar-open-btn');
+let sidebarCloseBtn = document.querySelector('.sidebar-close-btn');
+//sidebar-open-btn 클릭했을때 is-active 클래스 추가
+sidebarOpenBtn.addEventListener('click',function(){
+    sidebar.classList.add('is-active')
+})
+//sidebar-close-btn 클릭했을때 is-active 클래스 추가
+sidebarCloseBtn.addEventListener('click',function(){
+    sidebar.classList.remove('is-active')
 })
