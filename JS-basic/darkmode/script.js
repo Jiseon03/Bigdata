3e6df44e4d2d33dc7b9b581ed2f7d39528d9893c
Jiseon03 <wltjsqhgus0905@gmail.com>
2024-06-07 10:31:36 +0900

    📝 수정

--- JS-basic/darkmode/script.js
+++ JS-basic/darkmode/script.js
@@ -1,15 +1,20 @@
-let headerBtn = document.querySelector('.header-btn');
-let header = document.querySelector('header');
+let headerBtn = document.querySelector(".header-btn");
+function darkModeToggle() {
+    let btnTxt = headerBtn.innerHTML;
+    let header = document.querySelector("header");
+    //header-btn 을 클릭했을 때
+    headerBtn.addEventListener("click", function () {
+    if (btnTxt == "Dark") { //버튼 텍스트가 'Dark'일 경우
+        btnTxt = "Light";      
+        header.classList.add("dark");
+        headerBtn.classList.add("dark");
+    } 
+    else if (btnTxt == "Light") { //버튼 텍스트가 'Light'일 경우
+        btnTxt = "Dark";
+        header.classList.remove("dark");
+        headerBtn.classList.remove("dark");
+    }
+    });
+}
 
-headerBtn.addEventListener('click',function(){
-    if(headerBtn.innerHTML == 'Dark'){
-        headerBtn.innerHTML = 'Light';
-        header.classList.add('dark');
-        headerBtn.classList.add('dark');
-    }
-    else if(headerBtn.innerHTML == 'Light'){
-        headerBtn.innerHTML = 'Dark';
-        header.classList.remove('dark');
-        headerBtn.classList.remove('dark');
-    }
-})
+darkModeToggle();
