0c23c92f990f8c2ed4304d00ea2b1d7165a5eaa7
Jiseon03 <wltjsqhgus0905@gmail.com>
2024-06-07 11:10:31 +0900

    📝 다른 방법으로 구현

--- JS-basic/darkmode/script.js
+++ JS-basic/darkmode/script.js
@@ -1,20 +1,41 @@
 let headerBtn = document.querySelector(".header-btn");
+// function darkModeToggle() {
+//     let btnTxt = headerBtn.innerHTML;
+//     let header = document.querySelector("header");
+
+//     headerBtn.addEventListener("click", function () {
+//     if (btnTxt == "Dark") {
+//         btnTxt = "Light";
+//         header.classList.add("dark");
+//         headerBtn.classList.add("dark");
+//     }
+//     else{
+//         btnTxt = "Dark";
+//         header.classList.remove("dark");
+//         headerBtn.classList.remove("dark");
+//     }
+//     });
+// }
+
+
+
 function darkModeToggle() {
-    let btnTxt = headerBtn.innerHTML;
-    let header = document.querySelector("header");
-    //header-btn 을 클릭했을 때
-    headerBtn.addEventListener("click", function () {
-    if (btnTxt == "Dark") { //버튼 텍스트가 'Dark'일 경우
-        btnTxt = "Light";      
-        header.classList.add("dark");
-        headerBtn.classList.add("dark");
-    } 
-    else if (btnTxt == "Light") { //버튼 텍스트가 'Light'일 경우
-        btnTxt = "Dark";
-        header.classList.remove("dark");
-        headerBtn.classList.remove("dark");
+  let count = 0;
+  let btnTxt = headerBtn.innerHTML;
+  let header = document.querySelector("header");
+
+  headerBtn.addEventListener("click", function () {
+    count++;
+    if (count % 2 !== 0) { //홀수 일 때
+      btnTxt = "Light";
+      header.classList.add("dark");
+      headerBtn.classList.add("dark");
+    } else { //짝수 일 때
+      btnTxt = "Dark";
+      header.classList.remove("dark");
+      headerBtn.classList.remove("dark");
     }
-    });
+  });
 }
 
 darkModeToggle();
